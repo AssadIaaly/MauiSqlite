a3015c3213e1ab4608415e94d709a118a86f5361
Assad Iaaly <assad.iaaly@outlook.com>
2023-01-13 06:51:00 +0300

    Add Class Library

---
+++ MauiSqliteClassLibrary/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using MauiSqliteClassLibrary.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MauiSqliteClassLibrary
+{
+    public class ApplicationDbContext : DbContext
+    {
+        // Constructor with no argument is required and it is used when adding/removing migrations from class library
+        public ApplicationDbContext()
+        {
+        }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+            //Database.EnsureCreated();
+            Database.Migrate();
+        }
+
+        // It is required to override this method when adding/removing migrations from class library
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlite();
+
+        public DbSet<Post> Posts { get; set; }
+    }
+}
---
+++ MauiSqliteClassLibrary/Migrations/20230112045129_AddContentToPostTabel.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MauiSqliteClassLibrary.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddContentToPostTabel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Content",
+                table: "Posts",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Content",
+                table: "Posts");
+        }
+    }
+}
