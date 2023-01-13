8e39ed246e4db799cb6c6424c4f94e78a3e6cca5
Assad Iaaly <assad.iaaly@outlook.com>
2023-01-13 06:47:11 +0300

    Add project files.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,73 @@
+﻿using MauiSqliteClassLibrary.Models;
+using MauiSqliteClassLibrary;
+using Microsoft.EntityFrameworkCore;
+
+namespace MauiSqlite;
+
+public partial class MainPage : ContentPage
+{
+	int counter = 0;
+    private readonly ApplicationDbContext _context;
+
+    public MainPage(ApplicationDbContext context)
+	{
+		InitializeComponent();
+        _context = context;
+    }
+
+	private async void OnAddToDbClicked(object sender, EventArgs e)
+    {
+		counter++;
+        var newPost = new Post
+        {
+            Title = $"Post Number {counter}",
+            Content = $"Content for Post: {counter}"
+        };
+
+        try
+        {
+            await _context.Posts.AddAsync(newPost);
+            await _context.SaveChangesAsync();
+            ResultEditor.Text = $"Post {counter} Added successfully";
+        }
+        catch (Exception ex)
+        {
+            ResultEditor.Text = ex.Message;
+        }
+    }
+    private async void OnReadFromDbClicked(object sender, EventArgs e)
+    {
+
+        try
+        {
+
+            var posts = await _context.Posts.ToListAsync();
+            var text = "";
+            foreach (var post in posts)
+            {
+                text += $"Id:{post.Id} {post.Title} Content: {post.Content}" + Environment.NewLine;
+            }
+            ResultEditor.Text = text;
+        }
+        catch (Exception ex)
+        {
+            ResultEditor.Text = ex.Message;
+        }
+    }
+    private void OnClearTableClicked(object sender, EventArgs e)
+    {
+
+        try
+        {
+
+            _context.Posts.RemoveRange(_context.Posts.ToList());
+            _context.SaveChanges();
+            ResultEditor.Text = "Table Cleared";
+        }
+        catch (Exception ex)
+        {
+            ResultEditor.Text = ex.Message;
+        }
+    }
+}
+
---
+++ MauiProgram.cs
@@ -0,0 +1,48 @@
+﻿using MauiSqliteClassLibrary;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace MauiSqlite;
+
+public static class MauiProgram
+{
+	public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+		builder
+			.UseMauiApp<App>()
+			.ConfigureFonts(fonts =>
+			{
+				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+			});
+
+#if DEBUG
+		builder.Logging.AddDebug();
+#endif
+        builder.Services.AddSingleton<MainPage>();
+        builder.Services.AddDbContext<ApplicationDbContext>(
+            options => options.UseSqlite($"Filename={GetDatabasePath()}", x => x.MigrationsAssembly(nameof(MauiSqliteClassLibrary))));
+
+        return builder.Build();
+	}
+
+    public static string GetDatabasePath()
+    {
+        var databasePath = "";
+        var databaseName = "Maui.db3";
+
+        if (DeviceInfo.Platform == DevicePlatform.Android)
+        {
+            databasePath = Path.Combine(FileSystem.AppDataDirectory, databaseName);
+        }
+        if (DeviceInfo.Platform == DevicePlatform.iOS)
+        {
+            SQLitePCL.Batteries_V2.Init();
+            databasePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "..", "Library", databaseName); ;
+        }
+
+        return databasePath;
+
+    }
+}
